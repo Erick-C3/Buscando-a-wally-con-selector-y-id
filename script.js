27f098a546d1a427b358cbdef1333f62eec3e623
Erick <erick.nahuelpaiva.001@gmail.com>
2023-04-17 18:47:02 -0300

    encontrando a wally

---
+++ script.js
@@ -0,0 +1,29 @@
+const titulo = document.querySelector("");
+const imagen = document.querySelector("");
+const cuerpoPagina = document.querySelector("");
+const detalles = document.querySelector("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cuerpoPagina.style.backgroundColor = "#213f19";
+cuerpoPagina.style.color = "white";
+titulo.innerHTML = "Encontrando a id-wally";
+detalles.innerHTML = "Estas encontrando los id para ver a wally!"
+imagen.src = "https://media.tenor.com/6hZ2jBhHDKAAAAAM/deekaymotion-deekay.gif";
